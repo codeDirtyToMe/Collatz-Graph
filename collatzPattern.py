ec03e0421e7484c69e955de5101ee722ca14c8b4
codeDirtyToMe <32625084+codeDirtyToMe@users.noreply.github.com>
2017-11-05 20:55:05 -0500

    Update collatzPattern.py
    Started looking for a pattern concerning how many times a number approaches infinity and how many times it approaches 1 while going through the function. The changes are matched to the value on the graph. There's some interesting consistencies.

--- collatzPattern.py
+++ collatzPattern.py
@@ -1,39 +1,62 @@
 #!/usr/bin/python3.5
-import plotly
+import plotly, logging
 import plotly.graph_objs as go
-#By, codeDirtyToMe
+logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
 
 def plotOnGraph() :
-    limit = 500
+    limit = 100
     startingX = 2
-    averageY = [1]
-    averageX =[2]
     xValues = list()
     iterationValues = list()
+    signChangesPos = list()
+    signChangesNeg = list()
 
     while startingX <= limit:
         x = startingX
         counter = 0
-        xValues.insert(counter, startingX)
+        positiveSignChanges = int(0)
+        negativeSignchanges = int(0)
+
+        xValues.insert(counter, startingX) #List of values that been run.
+
         while x != 1:
             counter += 1
             if x % 2 != 0:
                 x = (x * 3) + 1
+                positiveSignChanges += 1
             else:
                 x = x / 2
-        #print("The #" + str(startingX) + " takes " + str(counter) + " times.")
+                negativeSignchanges -= 1
+
         iterationValues.insert(0, counter)
+        signChangesPos.insert(0, positiveSignChanges) #Towards infinity
+        signChangesNeg.insert(0, negativeSignchanges) #Towards 1
         startingX += 1
-    iterationTotal = int(len(iterationValues) + 1)
-    iterationAverage = float(sum(iterationValues) / len(iterationValues))
-    #print(iterationAverage)
+
+    logging.debug(iterationValues)
+    logging.debug("Length of positive sign changes list: " + str(len(signChangesPos)))
+    logging.debug(signChangesPos)
+    logging.debug("Length of negative sign changes list: " + str(len(signChangesNeg)))
+    logging.debug(signChangesNeg)
 
     trace1 = go.Scatter(
         x = xValues,
         y = iterationValues,
-        name = "Iterations",
+        name = "Iterations"
     )
-    data = [trace1]
+
+    trace2 = go.Scatter(
+        x = xValues,
+        y = signChangesPos,
+        name = "+ Changes"
+    )
+
+    trace3 = go.Scatter(
+        x = xValues,
+        y = signChangesNeg,
+        name = "- Changes"
+    )
+    data = [trace1, trace2, trace3]
     # Edit the layout
     layout = dict(title = 'Collatz Pattern',
                   xaxis = dict(title = 'Numerical Value'),
@@ -44,3 +67,4 @@ def plotOnGraph() :
     plotly.offline.plot(fig, filename='styled-line.html')
 
 plotOnGraph()
+
